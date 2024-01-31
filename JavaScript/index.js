df7086c6c7f6dce04c0c328777db8bfcf42becd5
Mayank Gupta <mayank.gupta.6.88@gmail.com>
2024-01-31 10:42:39 +0530

    Session Two

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+// Random Number between 1 to 10
+
+// var randomNumber = Math.floor(Math.random() * 10) + 1  //8
+
+// var randVal = Math.round(0 + (Math.random() * (10)));  //8
+
+// var random = Math.ceil(Math.random()*10);  //8
+
+var random = require('randomize');
+
+var randomNumber = random(100);
+
+debugger;
