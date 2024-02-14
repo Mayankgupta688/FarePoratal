158edd5e62eebb55e4d1c08b821f31791f08708f
Mayank Gupta <mayank.gupta.6.88@gmail.com>
2024-02-14 10:27:48 +0530

    Session 10

---
+++ redux-implementation/src/components/AppComponent.js
@@ -0,0 +1,3 @@
+export default function AppComponent() {
+    return <h1>This is the Redux Application</h1>
+}
---
+++ redux-implementation/src/index.js
@@ -0,0 +1,5 @@
+// https://prod.liveshare.vsengsaas.visualstudio.com/join?F0D100877297698478E73D67B352E843AC9A
+
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<AppComponent></AppComponent>, document.getElementById("root"));
